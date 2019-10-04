b387613a0d5e649de3df7ef8b9a68dad3cb1d44d
Mackenzie Miao Smith <smithmm4@s.dcsdk12.org>
2019-10-04 09:09:40 -0600

    Computer switching practice

--- git.rb
+++ git.rb
@@ -1,3 +1,3 @@
 print "Coding is fun!!!!!"
-print "shoot why did I forget my computer?"
 print "I have a spanish test today oh no"
+print "That was yesterday lame-o, today you have APHG and like a billion other exams."
